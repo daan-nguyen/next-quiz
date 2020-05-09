8895bce6d9d9fd9d69bc412e51bdc31d86805efa
Dan Nguyen <daan.nguyen@gmail.com>
2020-05-09 12:22:59 +1000

    Add files via upload

---
+++ components/Buzzer.tsx
@@ -0,0 +1,27 @@
+import { Card, Button } from "antd";
+import useSocket from "../utils/useSocket";
+import useKeyPress from "../utils/useKeypress";
+
+const Buzzer: React.FC<{ socket: any }> = ({ socket }) => {
+  const spacePress = useKeyPress(" ");
+
+  const buzzHandler = () => {
+    socket?.emit("buzz");
+  };
+
+  return (
+    <Card style={{ width: 400, textAlign: "center" }} title="Get ready...">
+      <Button
+        style={{ height: 300, width: 300, fontSize: "60px" }}
+        type="danger"
+        onClick={buzzHandler}
+      >
+        BUZZ
+      </Button>
+      <p>... or use your <strong>space bar</strong>!</p>
+      {spacePress && buzzHandler()}
+    </Card>
+  );
+};
+
+export default Buzzer;
